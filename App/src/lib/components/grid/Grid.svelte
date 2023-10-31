0643bc52d531bf132fc190a8ee6e5fe4ffdbd1da
Alexander <94375773+Alxlol@users.noreply.github.com>
2023-10-31 09:33:37 +0100

    handle choice

--- App/src/lib/components/grid/Grid.svelte
+++ App/src/lib/components/grid/Grid.svelte
@@ -1,14 +1,24 @@
 <script>
 	import GridCard from "./GridCard.svelte";
     import { gridCards } from './GridCards'
+    import { firstSelection, secondSelection } from '../../stores/gameState'
     
     let cards = [...gridCards, ...gridCards]
         .sort(() => Math.random() - 0.5)
     console.log(cards)
+
+    const handleChoice = (card) => {
+        console.log(card)
+    }
 </script>
 
 <div class="grid gap-2 grid-cols-4">
     {#each cards as card}
-        <GridCard {card} />
+        <GridCard 
+        {card}
+        flipped={card === $firstSelection || card === $secondSelection}
+        {handleChoice} 
+        
+        />
     {/each}
 </div>
--- App/src/lib/components/grid/GridCard.svelte
+++ App/src/lib/components/grid/GridCard.svelte
@@ -1,10 +1,14 @@
 <script>
     export let card;
     export let flipped = false;
+    export let handleChoice;
+    let handleClick = () => {
+        handleChoice(card)
+    }
 
 </script>
 
-<button class="w-24 h-24 bg-blue-500 rounded-md ">
+<button on:click={handleClick} class="w-24 h-24 bg-blue-500 rounded-md ">
     {#if flipped}
         {card.src}
     {/if}
