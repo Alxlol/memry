fff6d2eedfe9aae447277cfc7df17f42ae6c1a35
Alexander <94375773+Alxlol@users.noreply.github.com>
2023-11-17 00:04:23 +0100

    edit main menu

--- App/src/routes/+page.svelte
+++ App/src/routes/+page.svelte
@@ -6,6 +6,10 @@
     }
 </script>
 
+<div class="text-center mb-10">
+    <p class="text-6xl font-bold">MEMRY</p>
+    <p class="italic text-lg">by Alexander Friberg</p>
+</div>
 <button on:click={onClickPlay} class="bg-green-400 p-8 px-12 rounded-lg shadow-lg text-5xl font-bold">
     PLAY
 </button>
